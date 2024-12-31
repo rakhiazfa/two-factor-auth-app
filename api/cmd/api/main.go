274ff9842fadbec6d2886f2933da7f2f588ac902
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2025-01-01 06:55:58 +0700

    feat: added timezone configuration.

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/rakhiazfa/gin-boilerplate/config"
 	"github.com/rakhiazfa/gin-boilerplate/internal/wire"
@@ -12,9 +13,16 @@ import (
 func main() {
 	config.InitViper(".")
 
+	local, err := time.LoadLocation(viper.GetString("application.timezone"))
+	if err != nil {
+		log.Fatal("Failed to load location: ", err)
+	}
+
+	time.Local = local
+
 	addr := fmt.Sprintf("%s:%d", viper.GetString("application.host"), viper.GetInt("application.port"))
 
-	err := wire.NewApplication().Run(addr)
+	err = wire.NewApplication().Run(addr)
 	if err != nil {
 		log.Fatal("Failed to run application: ", err)
 	}
--- api/internal/infrastructures/postgres.go
+++ api/internal/infrastructures/postgres.go
@@ -12,12 +12,13 @@ import (
 
 func NewPostgresConnection() *gorm.DB {
 	dsn := fmt.Sprintf(
-		"host=%s port=%d dbname=%s user=%s password=%s sslmode=disable TimeZone=Asia/Jakarta",
+		"host=%s port=%d dbname=%s user=%s password=%s sslmode=disable TimeZone=%s",
 		viper.GetString("database.host"),
 		viper.GetInt("database.port"),
 		viper.GetString("database.name"),
 		viper.GetString("database.username"),
 		viper.GetString("database.password"),
+		viper.GetString("application.timezone"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
