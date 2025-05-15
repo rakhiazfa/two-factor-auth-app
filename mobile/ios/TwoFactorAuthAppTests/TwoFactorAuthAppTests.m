558ac4c5736849fe48e875c117c1e75b8063bb45
hiriski <hiriski@outlook.com>
2025-05-15 20:44:08 +0700

    chore(mobile app): update app name

--- mobile/ios/TwoFactorAuthAppTests/TwoFactorAuthAppTests.m
+++ mobile/ios/TwoFactorAuthAppTests/TwoFactorAuthAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Example2FAAppTests : XCTestCase
+@interface TwoFactorAuthAppTests : XCTestCase
 
 @end
 
-@implementation Example2FAAppTests
+@implementation TwoFactorAuthAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
--- mobile/src/screens/home.screen.tsx
+++ mobile/src/screens/home.screen.tsx
@@ -1,13 +1,32 @@
 import { ImageAssets } from '@/assets'
 import { pusher } from '@/services/pusher.service'
 import { PusherEvent } from '@pusher/pusher-websocket-react-native'
-import { useEffect } from 'react'
-import { Alert, Image, StatusBar, Text } from 'react-native'
+import { useCallback, useEffect, useMemo, useRef } from 'react'
+import { Alert, Button, Image, StatusBar, Text } from 'react-native'
 import { View } from 'react-native'
 
 import { AppConfig } from '@/config'
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetView,
+} from '@gorhom/bottom-sheet'
+import SampleBottomSheet from '@/components/SampleBottomSheet'
 
 const HomeScreen = () => {
+  // const bottomSheetRef = useRef<BottomSheetModal | null>(null)
+
+  // ref
+  const bottomSheetRef = useRef<BottomSheet | null>(null)
+  const bottomSheetRef2 = useRef<BottomSheet | null>(null)
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%', '90%'], [])
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index)
+  }, [])
+
   useEffect(() => {
     pusher.subscribe({
       channelName: 'auth',
@@ -28,6 +47,26 @@ const HomeScreen = () => {
         source={ImageAssets.img1}
         style={{ height: 200, width: 200, resizeMode: 'contain' }}
       />
+
+      <Button
+        title='Open bottom sheet'
+        onPress={() => bottomSheetRef.current?.snapToIndex(1)}
+      />
+      <SampleBottomSheet ref={bottomSheetRef} />
+      <Button
+        title='Open bottom sheet 2'
+        onPress={() => bottomSheetRef2.current?.snapToIndex(1)}
+      />
+
+      <BottomSheet
+        ref={bottomSheetRef2}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <BottomSheetView style={{}}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   )
 }
